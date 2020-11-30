829f3b48ff610926b2f79e4b24287923f49a3164
unknown <guilhermecsilva8@gmail.com>
2020-11-30 12:15:10 -0300

    Adicionando o contrutor da ProdutoEstoque

--- ProdutoEstoque.java
+++ ProdutoEstoque.java
@@ -1,3 +1,15 @@
-class ProdutoEstoque{
-    
+public class ProdutoEstoque{
+	
+	String nome;
+	double valor;
+	int qntd;
+
+	public ProdutoEstoque(String nome, int valor, int qntd){
+		this.nome = nome;
+		this.valor = valor;
+		this. qntd = qntd;
+	}
+
+	public ProdutoEstoque(){}
+
 }
